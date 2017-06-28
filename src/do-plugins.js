4a11f03cf4807ac101761720d355aff5dca3746a
fiftyfactorauthentication <powella@gilt.com>
2017-06-28 12:25:03 -0400

    fixes #12: remove QA-specific visitTime format

--- src/do-plugins.js
+++ src/do-plugins.js
@@ -80,12 +80,7 @@ module.exports = function doPlugins (app) {
 
   app.events = app.events || '';
 
-  if (pageData.site.is_production === 'true') {
-    visitTime = app.getTimeParting('f', '-5');
-  }
-  else if (pageData.site.is_production === 'false') {
-    visitTime = app.getSaksTime();
-  }
+  visitTime = app.getTimeParting('f', '-5');
 
   evar(8, visitTime);
   prop(33, siteCatalystVersion);
